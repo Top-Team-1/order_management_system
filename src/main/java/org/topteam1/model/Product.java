ec2d99f7a47c5d56de1d5ff8c935b96d6a3a08be
Алексей <sayhell042@gmail.com>
2024-12-15 23:34:31 +0700

    add-new-classes

---
+++ src/main/java/org/topteam1/model/Product.java
@@ -0,0 +1,67 @@
+package org.topteam1.model;
+
+import java.util.Objects;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Integer price;
+    private String category;
+
+    public Product(Integer id, String name, Integer price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price) && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + id + " " + name + " " + price + " " + category +"}";
+    }
+}
