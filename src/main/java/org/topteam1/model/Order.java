8fdbe9f4de0e9fcfc1837572c4fa4f3531c6541b
Alexsskill <harckovaleks@yandex.ru>
2024-12-16 22:42:36 +0400

    Add new class Order

---
+++ src/main/java/org/topteam1/model/Order.java
@@ -0,0 +1,72 @@
+package org.topteam1.model;
+
+import java.util.Objects;
+
+public class Order {
+    private Integer id;
+    private Customer customer;
+    private Product product;
+    private String status;
+
+    public Order(Integer id, Customer customer, Product product, String status) {
+        this.id = id;
+        this.customer = customer;
+        this.product = product;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(product, order.product) && Objects.equals(status, order.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, product, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", product=" + product +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
